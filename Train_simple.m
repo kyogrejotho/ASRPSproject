5f78340c202fc504e7d845c5107c10ba98819b3f
David Buaron <jdarbuaron@gmail.com>
2024-11-13 00:23:46 +0100

    Some GUI

--- Train_simple.m
+++ Train_simple.m
@@ -1,21 +1,46 @@
 clc;
 clear;
-% Pref = '755'; % in kW
-% Vcat = '390'; % in V
-% prompt = {'Enter catenary power in kW:','Enter catenary voltage in V:'};
-% dlgtitle = 'Train Parameters';
-% fieldsize = [1 45; 1 45];
-% definput = {Pref,Vcat};
-% answer = inputdlg(prompt,dlgtitle,fieldsize,definput);
+eff = 0.9;
+%Pref = '755'; % in kW
+%Vcat = '390'; % in V
+%withOCP = false; 
+prompt = {'Enter reference power in kW:','Enter catenary voltage in V:', 'Consider Overcurrent Protection? Y/N'};
+dlgtitle = 'Train Parameters';
+fieldsize = [1 45; 1 45;1 45];
+definput = {'755','390','N'};
+vpAns = inputdlg(prompt,dlgtitle,fieldsize,definput);
+strPref = vpAns{1}; % can get rid of this, refer to ans array in fxn calls
+strVcat = vpAns{2};
+strOCP = vpAns{3};
+
+if checkNum(strPref)
+    Pref = str2double(strPref);
+end
+
+if checkNum(strVcat)
+    Vcat = str2double(strVcat);
+end
+
+if checkOCP(strOCP)
+    prompt = {'Enter minimum voltage in V','Enter maximum voltage in V:'};
+    dlgtitle = 'Set Voltage Limits';
+    fieldsize = [1 45; 1 45];
+    definput = {'550','600'};
+    vMinMaxAns = inputdlg(prompt,dlgtitle,fieldsize,definput);
+    
+    Train_protection(Pref,eff,Vcat)
+    
+end
+
 eff = 0.9; % 0 - 1
 %ex1 = Train_simple_fxn (500,eff,700)
-[Psupp1,Pnosupp1] = Train_protection(650,eff,650)
-[Psupp2,Pnosupp2] = Train_protection(650,eff,575)
-[Psupp3,Pnosupp3] = Train_protection(650,eff,560)
-[Psupp4,Pnosupp4] = Train_protection(650,eff,400)
-[Psupp5,Pnosupp5] = Train_protection(650,eff,550)
-[Psupp6,Pnosupp6] = Train_protection(650,eff,600)
-[Psupp7,Pnosupp7] = Train_protection(-650,eff,600)
+% [Psupp1,Pnosupp1] = Train_protection(650,eff,650)
+% [Psupp2,Pnosupp2] = Train_protection(650,eff,575)
+% [Psupp3,Pnosupp3] = Train_protection(650,eff,560)
+% [Psupp4,Pnosupp4] = Train_protection(650,eff,400)
+% [Psupp5,Pnosupp5] = Train_protection(650,eff,550)
+% [Psupp6,Pnosupp6] = Train_protection(650,eff,600)
+% [Psupp7,Pnosupp7] = Train_protection(-650,eff,600)
 
 function [Pcat] = Train_simple_fxn(Pref,eff, Vcat)
 %   Detailed explanation goes here
@@ -30,12 +55,10 @@ end
 
 function [Psupp,Pnosupp] =  Train_protection(Pref,eff,Vcat)
 
-    v1 = 550;
+    v1 = 550; % hardcoded for now
     v2 = 600;
 
     Pcat = Pref/eff;
-    Psupp = (Pcat/(v2-v1))*(Vcat-v1);
-    Pnosupp = Pref-(Psupp*eff);
     if Vcat > v2 % limit Psupply to Pcatenary
         Psupp = Pcat;
         Pnosupp = Pref-(Psupp*eff); % usual computation;
@@ -45,6 +68,32 @@ function [Psupp,Pnosupp] =  Train_protection(Pref,eff,Vcat)
     elseif Pcat < 0 % if negative, we are in traction and we have no supply power, Psupp = Pref*eff
         Psupp = Pref*eff;
         Pnosupp = 0;
+    else
+        Psupp = (Pcat/(v2-v1))*(Vcat-v1);
+        Pnosupp = Pref-(Psupp*eff);
     end
 end
 
+function withOCP = checkOCP(strOCP)
+    if strlength(strOCP) > 1
+        withOCP = 0;
+        uiwait(warndlg('Input too long!'));
+    elseif strcmpi('y',strOCP)
+        withOCP = 1;
+    elseif strcmpi('n',strOCP)
+        withOCP = 1
+    else
+        withOCP = 0;
+        uiwait(warnndlg('Invalid input! Please input only Y or N'));
+    end
+end
+
+function isNum = checkNum(numInput)
+    testNum = str2double(numInput);
+    if isnan(testNum)
+        isNum = 0;
+        uiwait(warnndlg('Invalid input! Please numerical values only for voltage or power'));
+    else
+        isNum = 1;
+    end
+end
