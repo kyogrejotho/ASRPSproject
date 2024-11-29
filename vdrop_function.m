5d768c1197542515d4ed321b0c315b27dfc2084f
Pchai98 <pch.rochananak@gmail.com>
2024-11-29 23:01:06 +0100

    TPSS loss cal

--- vdrop_function.m
+++ vdrop_function.m
@@ -5,22 +5,30 @@ Rrail = 0.014;
 % Rline = (18.8/(Dcontact*wear_factor+Dmessenger)+Rrail); %Resistance from TSS to train ohm/km
 Rline = 0.051;
 R = Rrail+Rline;
-R_TPSS = 228e-3;
 
-Pcat = 650e3;
+Pcat = 722e3;
 Vcat = 550;
-TPSSx1 = 0;
-TPSSx2 = 3;
-pk = 2;
-
-bilateral(Pcat,Vcat,pk,TPSSx1,TPSSx2,R);
+TPSSx1 = 0; % Distance Substation1
+TPSSx2 = 3; % Distance Substation2 
+pk = 2; % Train position
 
 
-% function [PTPSSloss] = TPSSloss()
-% 
-% 
-% 
-% end
+
+[Vdrop,ITPSS1,ITPSS2] = bilateral(Pcat,Vcat,pk,TPSSx1,TPSSx2,R);
+TPSSloss(ITPSS1)
+
+function PTPSSloss = TPSSloss(ITPSS)
+VratedTR = 750;
+PratedTR = 0.35e6;
+Vsc_percent = 8; % Short circuit voltage percentage
+Rf_TPSS = 228e-3; % Substation converter resistance
+
+IratedTR = PratedTR/VratedTR;
+ZTR = Vsc_percent * VratedTR/(100*IratedTR); % Short-circuit Impedance TR
+
+PTPSSloss = (ITPSS^2*ZTR)+(ITPSS^2*Rf_TPSS); % Total loss in TPSS
+
+end
 
 function [Vdrop] = unilateral(Pcat,Vcat,pk,TPSSx1,R)
 
